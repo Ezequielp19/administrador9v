e3545fca56593193ff65155a29a2b3b8b2eab3ee
Ezequielp19 <85073571+Ezequielp19@users.noreply.github.com>
2024-05-17 22:02:19 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ admin12/src/app/models/producto.ts
@@ -0,0 +1,10 @@
+export interface Producto {
+  _id?: string; // Podrías tener un identificador único en el frontend si lo necesitas
+  nombre: string;
+  descripcion: string;
+  imagen?: string;
+  precio: number;
+  descuento?: number;
+  precioFinal: number;
+  categorias: string[]; 
+}
